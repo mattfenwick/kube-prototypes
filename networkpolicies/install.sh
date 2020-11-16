fd5b45a07a37041e8ab228ac34f20fbc24adffca
Matthew Fenwick <mfenwick100@gmail.com>
2020-11-16 07:26:30 -0500

    add netpol examples

--- networkpolicies/install.sh
+++ networkpolicies/install.sh
@@ -4,4 +4,4 @@ kubectl apply -n $NS -f api-allow.yaml
 kubectl apply -n $NS -f blackduck-deny-all.yaml
 kubectl apply -n $NS -f deny-all.yaml
 kubectl apply -n $NS -f web-allow-all.yaml
-kubectl apply -n $NS -f web-allow-all-2.yaml
+kubectl apply -n $NS -f web-deny-all.yaml
